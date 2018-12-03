9745917d777a1d089145bbe674fa0c0854d25422
Mike Pruett <mikepruett3@gmail.com>
2018-12-02 21:34:02 -0600

    something else to try!

--- backup.sh
+++ backup.sh
@@ -3,6 +3,6 @@
 Container="Terraria"
 GameBackups="/data/game-backups/$Container/"
 TimeStamp=$(date +"%m-%d-%Y_%H-%M-%S")
-FileName="$Container_$TimeStamp.tar.gz"
+FileName="$Container-$TimeStamp.tar.gz"
 
-docker run --rm --volumes-from ${Container} -v ${GameBackups}:/backup ubuntu tar cvf /backup/${$Container_$TimeStamp.tar.gz} /app/Worlds
+docker run --rm --volumes-from ${Container} -v ${GameBackups}:/backup ubuntu tar cvf /backup/${$Container-$TimeStamp.tar.gz} /app/Worlds
