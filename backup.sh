84bcb1b4e2f9d0d855b07e0e0833b121dcaf12a1
Mike Pruett <mikepruett3@gmail.com>
2018-12-03 00:09:57 -0600

    unary operator expected! ([[ ]])

--- backup.sh
+++ backup.sh
@@ -8,7 +8,7 @@ containers() {
     printf '%s\n' "${Containers[@]}"
     echo ""
     read -p "Which Container? > " Container
-    if [ $(contains "${Containers[@]}" "$Container") == "n" ]; then
+    if [[ $(contains "${Containers[@]}" "$Container") == "n" ]]; then
     #if [[ "$Container" != "$(${Containers[@]})" ]]; then
         echo "$Container not a valid Container!"
         exit 1
