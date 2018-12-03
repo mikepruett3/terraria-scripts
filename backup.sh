7c9baa5a6941c8fc3449e7a7d1194fe1f39a21ff
Mike Pruett <mikepruett3@gmail.com>
2018-12-02 21:59:10 -0600

    Added chown to the end of the script

--- backup.sh
+++ backup.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 
 Container="Terraria"
+User="media"
+Group="media"
 GameBackups="/data/game-backups/$Container/"
 TimeStamp=$(date +"%m-%d-%Y_%H-%M-%S")
-FileName="$Container-$TimeStamp.tar.gz"
 
+# Create Backup of Volume
 docker run --rm --volumes-from $Container -v $GameBackups:/backup ubuntu tar cvf "/backup/$Container-$TimeStamp.tar.gz" /app/Worlds
+
+# Change Owner of Archive
+sudo chown $User:$Group "$GameBackups/$Container-$TimeStamp.tar.gz"
